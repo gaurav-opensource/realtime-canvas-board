fc8a76c120d2d20953b7184232122a3258abfb26
gaurav-opensource <gauravopensource95@gmail.com>
2026-04-18 02:53:49 +0530

    Initial project setup with frontend and backend structure

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io(import.meta.env.VITE_BACKEND_URL);
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["polling", "websocket"]
+});
 console.log("BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
